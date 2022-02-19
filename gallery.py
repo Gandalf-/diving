9d6e6743179f984cbeee557f90da7f819ff74567
Gandalf- <austin.voecks@gmail.com>
2022-02-18 19:36:32 -0800

    move link_check to verify.py

--- gallery.py
+++ gallery.py
@@ -5,8 +5,6 @@ search through diving pictures to produce a 'taxonomy tree', then convert that
 tree into HTML pages for diving.anardil.net
 '''
 
-import re
-import os
 import sys
 import multiprocessing
 from datetime import datetime
@@ -18,6 +16,7 @@ import taxonomy
 import timeline
 import util.collection as collection
 import util.static as static
+import util.verify as verify
 
 from detective import javascript as game
 from hypertext import Where, Side
@@ -312,47 +311,9 @@ def _find_by_path(tree, needle):
     return None
 
 
-def _find_links():
-    """check the html directory for internal links"""
-
-    def extract_from(fd):
-        """get links from a file"""
-        for line in fd:
-            if 'href' not in line:
-                continue
-
-            for link in re.findall(r'href=\"(.+?)\"', line):
-                if link.startswith('http'):
-                    continue
-
-                link = link[1:]
-                yield path, link
-
-    for directory in ('taxonomy', 'gallery', 'sites'):
-        for filename in os.listdir(directory):
-            if not filename.endswith(".html"):
-                continue
-
-            path = os.path.join(directory, filename)
-            with open(path) as fd:
-                yield from extract_from(fd)
-
-
-# INFORMATIONAL
-
-
-def link_check():
-    """check the html directory for broken links by extracting all the
-    internal links from the written files and looking for those as paths
-    """
-    for path, link in _find_links():
-        if not os.path.exists(link):
-            print('broken', link, 'in', path)
-
-
 if not sys.flags.interactive and __name__ == "__main__":
     if len(sys.argv) == 2:
         root = sys.argv[1]
 
     write_all_html()
-    link_check()
+    verify.link_check()
--- util/verify.py
+++ util/verify.py
@@ -3,3 +3,44 @@
 '''
 Check for broken links, misspelled names, and more
 '''
+
+import os
+import re
+
+
+def link_check():
+    """check the html directory for broken links by extracting all the
+    internal links from the written files and looking for those as paths
+    """
+    for path, link in _find_links():
+        if not os.path.exists(link):
+            print('broken', link, 'in', path)
+
+
+# PRIVATE
+
+
+def _find_links():
+    """check the html directory for internal links"""
+
+    def extract_from(fd):
+        """get links from a file"""
+        for line in fd:
+            if 'href' not in line:
+                continue
+
+            for link in re.findall(r'href=\"(.+?)\"', line):
+                if link.startswith('http'):
+                    continue
+
+                link = link[1:]
+                yield path, link
+
+    for directory in ('taxonomy', 'gallery', 'sites'):
+        for filename in os.listdir(directory):
+            if not filename.endswith(".html"):
+                continue
+
+            path = os.path.join(directory, filename)
+            with open(path) as fd:
+                yield from extract_from(fd)
