416a0987cbe129f08f4a8f316c1a9a65bdf44863
Gandalf- <austin@anardil.net>
2023-07-23 17:45:53 -0700

    refactor search.py

--- search.py
+++ search.py
@@ -2,41 +2,39 @@
 
 import os
 
+from typing import Iterable
+
 
 def write_search_data() -> None:
     '''JSON site map for gallerySearch'''
-    gal_pages = []
-    for page in os.listdir('gallery'):
-        if not page.endswith('.html'):
-            continue
-
-        prefixes = ('index', 'various', 'juvenile')
-        if any(page.startswith(prefix) for prefix in prefixes):
-            continue
-
-        gal_pages.append(_cleanup(page))
-
-    tax_pages = []
-    for page in os.listdir('taxonomy'):
-        if not page.endswith('.html'):
-            continue
-
-        prefixes = ('index', 'various', 'juvenile')
-        if any(page.startswith(prefix) for prefix in prefixes):
-            continue
-
-        tax_pages.append(_cleanup(page))
+    gal_pages = _reader('gallery')
+    tax_pages = _reader('taxonomy')
 
     with open('search-data.js', 'w') as fd:
         fd.write('var gallery_pages = [')
         fd.write(','.join(gal_pages))
-        fd.write('];')
+        fd.write('];\n')
 
         fd.write('var taxonomy_pages = [')
         fd.write(','.join(tax_pages))
-        fd.write('];')
+        fd.write('];\n')
+
+
+# PRIVATE
 
 
 def _cleanup(name: str) -> str:
     name = name.lower().replace('.html', '').replace('-', ' ')
     return f'"{name}"'
+
+
+def _reader(path: str) -> Iterable[str]:
+    for page in os.listdir(path):
+        if not page.endswith('.html'):
+            continue
+
+        prefixes = ('index', 'various', 'juvenile')
+        if any(page.startswith(prefix) for prefix in prefixes):
+            continue
+
+        yield _cleanup(page)
